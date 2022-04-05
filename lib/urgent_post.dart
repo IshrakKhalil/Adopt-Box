810ce408d2de8c78afc48b43297a1bb1298b20b4
Ishrak <ishrak.office@gmail.com>
2022-04-05 14:18:19 +0600

    Box - Style

--- lib/urgent_post.dart
+++ lib/urgent_post.dart
@@ -13,77 +13,34 @@ class UrgentPost_Box extends StatelessWidget {
       body: Center(
         child: Container(
           height: 106,
-          width: double.maxFinite,
+          width: 350,
           decoration: BoxDecoration(
             color: Color(0xffA2FECF),
             borderRadius: BorderRadius.circular(7),
           ),
           child: Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: Column(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                //Header Of Box
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Kitty Sadia",
-                      style: TextStyle(
-                        fontSize: 14,
-                      ),
-                    ),
-                    Container(
-                      height: 10,
-                      width: 10,
-                      decoration: BoxDecoration(
-                        color: Color(0xffFFDD99),
-                        borderRadius: BorderRadius.circular(7),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(2.0),
-                        child: SvgPicture.asset(
-                          "assets/icon/heart.svg",
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                //Image and other Information
                 Row(
                   children: [
                     Container(
-                      height: 60,
-                      width: 65,
+                      height: 95,
+                      width: 70,
                       color: Color.fromARGB(255, 226, 113, 20),
                     ),
                     SizedBox(
-                      width: 5,
+                      width: 10,
                     ),
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        //Location
-                        Row(
-                          children: [
-                            Container(
-                              width: 6,
-                              height: 6,
-                              color: Color.fromARGB(255, 226, 113, 20),
-                            ),
-                            SizedBox(
-                              width: 5,
-                            ),
-                            Text(
-                              "Dhaka, Dhanmondi",
-                              style: TextStyle(
-                                fontSize: 8,
-                              ),
-                            ),
-                          ],
+                        Text(
+                          "Kitty Sadia",
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
                         ),
                         SizedBox(
                           height: 5,
@@ -97,7 +54,6 @@ class UrgentPost_Box extends StatelessWidget {
                         SizedBox(
                           height: 5,
                         ),
-                        //Gender and Age
                         Row(
                           children: [
                             Text(
@@ -108,9 +64,6 @@ class UrgentPost_Box extends StatelessWidget {
                             ),
                             SizedBox(
                               width: 5,
-                            ),
-                            SvgPicture.asset(
-                              "assets/icon/line.svg",
                             ),
                             SizedBox(
                               width: 5,
@@ -123,31 +76,51 @@ class UrgentPost_Box extends StatelessWidget {
                             ),
                           ],
                         ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        //Button
-                        InkWell(
-                          onTap: () {},
-                          child: Container(
-                            width: 50,
-                            height: 20,
-                            decoration: BoxDecoration(
-                              color: Color(0xffFFDD99),
-                              borderRadius: BorderRadius.circular(3),
+                        RichText(
+                          text: TextSpan(
+                            text: ''''The loss of any loved one, regardless of 
+// they are a human or animal Death and the emotions
+// it brings are never easy to deal with. ''',
+                            style: TextStyle(
+                              fontSize: 6,
+                              color: Colors.black,
                             ),
-                            child: Center(
-                              child: Text(
-                                "Adopt Box",
+                            children: const <TextSpan>[
+                              TextSpan(
+                                text: 'More',
                                 style: TextStyle(
-                                  fontSize: 8,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 6,
                                   color: Colors.black,
                                 ),
                               ),
+                            ],
+                          ),
+                        ),
+//
+                      ],
+                    ),
+                    SizedBox(
+                      width: 60,
+                    ),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                        height: 10,
+                        width: 40,
+                        decoration: BoxDecoration(
+                          color: Color(0xffFFDD99),
+                          borderRadius: BorderRadius.circular(7),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Urgent",
+                            style: TextStyle(
+                              fontSize: 8,
                             ),
                           ),
                         ),
-                      ],
+                      ),
                     ),
                   ],
                 ),
